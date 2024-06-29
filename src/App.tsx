732b788c3e13316bd3a50b0b7cf02abeaef5f159
Fardin Omor Afnan <afnan.eu.cse@gmail.com>
2024-06-29 12:07:07 +0600

    refactor: App.ts for implementing protected route

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Outlet,
+    Navigate,
+} from "react-router-dom";
 import Main from "./components/layout/Main";
 import Dashboard from "./components/pages/Dashboard";
 import Products from "./components/pages/Products";
 import NotFound from "./components/pages/NotFound";
 import ProductDetails from "./components/pages/Products/ProductDetails";
 import UpdateProduct from "./components/pages/Products/UpdateProduct";
+import Login from "./components/Auth/Login";
 
+const ProtectedRoutes = () => {
+    const isAuth = true;
+
+    return isAuth ? (
+        <Main>
+            <Outlet />
+        </Main>
+    ) : (
+        <Navigate to="/login" />
+    );
+};
 function App() {
     return (
         <Router>
-            <Main>
-                <Routes>
+            <Routes>
+                <Route path="/login" element={<Login />} />
+                <Route element={<ProtectedRoutes />}>
                     <Route path="/" element={<Dashboard />} />{" "}
                     <Route path="/products" element={<Products />} />{" "}
                     <Route
@@ -21,9 +40,9 @@ function App() {
                         path="/product/edit/:productId"
                         element={<UpdateProduct />}
                     />
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </Main>
+                </Route>
+                <Route path="*" element={<NotFound />} />
+            </Routes>
         </Router>
     );
 }
--- src/components/layout/Main.tsx
+++ src/components/layout/Main.tsx
@@ -4,12 +4,23 @@ import {
     ProductFilled,
     PlusSquareFilled,
     OrderedListOutlined,
+    UserOutlined,
+    ArrowLeftOutlined,
 } from "@ant-design/icons";
-import { Layout, Menu, theme } from "antd";
-import { Link } from "react-router-dom";
+import {
+    Breadcrumb,
+    Button,
+    Dropdown,
+    Flex,
+    Layout,
+    Menu,
+    Typography,
+    theme,
+} from "antd";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 const { Header, Content, Footer, Sider } = Layout;
-
+const { Title } = Typography;
 const items = [
     {
         key: "dashboard",
@@ -35,11 +46,43 @@ const items = [
         ],
     },
 ];
+const dropDownItems = [
+    {
+        key: "Profile",
+        icon: React.createElement(ProductFilled),
+        label: <Link to="/profile">Profile</Link>,
+    },
+
+    {
+        label: "Logout",
+        key: "SubMenu",
+        icon: React.createElement(ProductFilled),
+    },
+];
+
+const breadcrumbLinks: Record<string, string> = {
+    "/": "Dashboard",
+    "/products": "Products",
+    "/add-product": "Add Product",
+    "/profile": "Profile",
+};
 
 const App = ({ children }: { children: ReactNode }) => {
+    const navigate = useNavigate();
+    const location = useLocation();
     const {
         token: { colorBgContainer, borderRadiusLG },
     } = theme.useToken();
+
+    const pathSnippets = location.pathname.split("/").filter((i) => i);
+    const breadcrumbItems = pathSnippets.map((_, index) => {
+        const url = `/${pathSnippets.slice(0, index + 1).join("/")}`;
+        return (
+            <Breadcrumb.Item key={url}>
+                <Link to={url}>{breadcrumbLinks[url]}</Link>
+            </Breadcrumb.Item>
+        );
+    });
 
     return (
         <Layout
@@ -47,17 +90,64 @@ const App = ({ children }: { children: ReactNode }) => {
                 overflow: "hidden",
             }}
         >
-            <Sider breakpoint="lg" collapsedWidth="0">
-                <div className="demo-logo-vertical" />
+            <Sider
+                style={{
+                    background: "#fff",
+                }}
+                defaultCollapsed
+                breakpoint="lg"
+                collapsedWidth="0"
+            >
+                <Title
+                    style={{
+                        textAlign: "center",
+                        marginTop: "0",
+                    }}
+                >
+                    360
+                    <span
+                        style={{
+                            color: "tomato",
+                        }}
+                    >
+                        ICT
+                    </span>
+                </Title>
                 <Menu
-                    theme="dark"
+                    theme="light"
                     mode="inline"
                     defaultSelectedKeys={["4"]}
                     items={items}
                 />
             </Sider>
             <Layout>
-                <Header style={{ padding: 0, background: colorBgContainer }} />
+                <Header
+                    style={{
+                        padding: "0 16px",
+                        background: colorBgContainer,
+                    }}
+                >
+                    <Flex align="center" justify="space-between">
+                        <Flex align="center">
+                            <Button
+                                type="text"
+                                icon={<ArrowLeftOutlined />}
+                                onClick={() => navigate(-1)}
+                                style={{ marginRight: "16px" }}
+                            />
+                            <Breadcrumb>{breadcrumbItems}</Breadcrumb>
+                        </Flex>
+                        <Dropdown
+                            menu={{ items: dropDownItems }}
+                            placement="bottomRight"
+                            arrow
+                        >
+                            <UserOutlined
+                                style={{ fontSize: "20px", cursor: "pointer" }}
+                            />
+                        </Dropdown>
+                    </Flex>
+                </Header>
                 <Content
                     style={{
                         margin: "24px 16px 0",
